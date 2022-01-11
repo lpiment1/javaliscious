23bdcdb252d7acb9049b66e8d7e33e4a4d068fa6
lpiment1 <44218362+lpiment1@users.noreply.github.com>
2022-01-11 15:45:10 -0800

    Uploaded javaliscious main root folder
    Uploaded javaliscious main root folder.

---
+++ javaliscious_website/CNIT_195-main/js/menu_list.js
@@ -0,0 +1,138 @@
+var menu_list = [
+
+    {
+        "id": 1,
+        "category":"drink", 
+        "name": "House Coffee",
+        "description": "A premium blend of smokey rich, fair trade and organic coffee.",
+        "Price": ["Small - $2.25","Medium - $3.00","Large - $3.25"],
+        "Creams": ["None","1","2","3"],
+        "Sugars": ["None","1","2","3"],
+        "Type": ["Hot","Iced"],
+        "image_url": "images/coffee.png"
+    },
+
+    {
+        "id": 2,
+        "name": "Americano",
+        "category":"drink", 
+        "description": "Our dark rich espresso culminating in this wonderfully rich cup with depth and nuance.",
+        "Price": ["Small - $3.25","Medium - $4.00","Large - $4.25"],
+        "Creams": ["None","1","2","3"],
+        "Sugars": ["None","1","2","3"],
+        "Type": ["Hot","Iced"],
+        "image_url": "images/americano.png"
+    },
+
+    {
+        "id": 3,
+        "name": "Latte",
+        "category":"drink", 
+        "description": "Our dark rich espresso balanced with steamed milk and a light layer of foam.",
+        "Price": ["Small - $4.25","Medium - $5.00","Large - $5.25"],
+        "Milk": ["Whole","LowFat","Nonfat","Almond"],
+        "Sugars": ["None","1","2","3"],
+        "Type": ["Hot","Iced","Blended"],
+        "image_url": "images/latte.png"
+    },
+
+    {
+        "id": 4,
+        "name": "Cappuccino",
+        "category":"drink", 
+        "description": "Our dark, rich espresso and steamed milk topped with a smoothed and stretched layer of thick milk foam.",
+        "Price": ["Small - $4.25","Medium - $5.00","Large - $5.25"],
+        "Milk": ["Whole","LowFat","Nonfat","Almond"],
+        "Sugars": ["0","1","2","3"],
+        "Type": ["Hot","Iced"],
+        "image_url": "images/cappuccino.png"
+    },
+
+    {
+        "id": 5,
+        "name": "Mocha",
+        "category":"drink", 
+        "description": "Our full-bodied espresso combined with mocha sauce and steamed milk, topped with whipped cream.",
+        "Price": ["Small - $5.25","Medium - $6.00","Large - $6.25"],
+        "Milk": ["Whole","LowFat","Nonfat","Almond"],
+        "Type": ["Hot","Iced","Blended"],
+        "Whipped_Cream": ["Yes","No"],
+        "image_url": "images/mocha.png"
+    },
+
+    {
+        "id": 6,
+        "name": "Bagel",
+        "category":"food", 
+        "description": "Whether eaten on their own or with a schmear of cream cheese, these bagels are a true taste of New York!",
+        "Price": ["Plain - $3.00","Popppy Seed - $3.50","Cinnamon Raisin - $3.75"],
+        "Spread": ["Butter","Cream Cheese","Peanut Butter"],
+        "Jam": ["Strawberry","Grape","Peach"],
+        "Toasted": ["Yes", "No"],
+        "image_url": "images/bagels.png"
+    },
+
+    {
+        "id": 7,
+        "name": "Crousant",
+        "category":"food", 
+        "description": "Our classic croissant is made with real butter to create a golden, crunchy top with soft, flaky layers inside. ",
+        "Price": ["Plain - $2.75","Choclate - $3.00","Almond - $3.25"],
+        "Toasted": ["Yes","No"],
+        "image_url": "images/crousants.png"
+    },
+
+    {
+        "id": 8,
+        "name": "Muffin",
+        "category":"food", 
+        "description": "Our delicious muffins are both sweet and moist whether you enjoy chocolate chip, blueberry , or pumkin",
+        "Price": ["Choclate Chip - $2.25","Blueberry - $2.50","Pumpkin - $2.75"],
+        "Heated": ["Yes","No"],
+        "image_url": "images/muffins.png"
+    },
+
+    {
+        "id": 9,
+        "name": "Scone",
+        "category":"food", 
+        "description": "A traditional scone with chocolate chips, blueberries, or pumpkin is a wholesome treat.",
+        "Price": ["Choclate Chip - $2.75","Blueberry - $3.00","Pumpkin - $3.25"],
+        "Heated": ["Yes","No"],
+        "image_url": "images/scones.png"
+    },
+    
+    
+    {
+        "id": 10,
+        "name": "T Shirt",
+        "category":"merch",
+        "description": "Our Famous T-Shirt, ethically-sourced, 100% Cotton.",
+        "Price": ["Standard - $17.00", "Collector's - $20.00"],
+        "Type": ["Small","Medium","Large","Extra Large"],
+        "image_url": "images/t-shirt.png"
+    },
+    
+    {
+        "id": 11,
+        "name": "Gift Card",
+        "category":"merch", 
+        "description": "For Family, Friends, and all lovers of Javalicious",
+        "Price": ["$5.00","$10.00","$20.00","$50.00"],
+        "image_url": "images/gift-card.png"
+    },
+    
+        {
+        "id": 12,
+        "name": "Granola",
+        "category":"food", 
+        "description": "Fresh, toasted oats, nuts and dried fruit.",
+        "Price": ["Small - $5.00","Medium - $10.00","Large - $15.00"],
+        "Milk": ["None","2%","Whole","Almond","Soy"],
+        "Fruit": ["Blueberries","Strawberries","Banana"],
+        "image_url": "images/granola.jpg"
+        } 
+
+];
+
+
